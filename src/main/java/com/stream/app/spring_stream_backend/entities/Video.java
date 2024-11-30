a7571d55538829e12c143cfd53959f59f22ca801
omarkamal62 <okamal34@gmail.com>
2024-11-30 04:49:00 +0200

    first commit

---
+++ src/main/java/com/stream/app/spring_stream_backend/entities/Video.java
@@ -0,0 +1,28 @@
+package com.stream.app.spring_stream_backend.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@Table(name="yt_videos")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Video {
+
+    @Id
+    private String videoId;
+    private String title;
+    private String description;
+    private String contentType;
+    private String filePath;
+
+    @ManyToOne
+    private Course course;
+
+}
---
+++ src/main/java/com/stream/app/spring_stream_backend/services/Impl/VideoServiceImpl.java
@@ -0,0 +1,168 @@
+package com.stream.app.spring_stream_backend.services.Impl;
+
+import com.stream.app.spring_stream_backend.entities.Video;
+import com.stream.app.spring_stream_backend.repositories.VideoRepository;
+import com.stream.app.spring_stream_backend.services.VideoService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
+
+@Service
+public class VideoServiceImpl implements VideoService {
+
+    @Value("${files.video}")
+    String DIR;
+
+    @Value("${file.video.hsl}")
+    String HSL_DIR;
+
+    private VideoRepository videoRepository;
+
+    public VideoServiceImpl(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    @PostConstruct
+    public void init () {
+        File file = new File(DIR);
+        File file1 = new File(HSL_DIR);
+
+        try {
+            Files.createDirectories(Paths.get(HSL_DIR));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(!file1.exists()) {
+            file1.mkdir();
+            System.out.println("Folder Created");
+        } else {
+            System.out.println("Folder already created");
+        }
+
+        if(!file.exists()) {
+            file.mkdir();
+            System.out.println("Folder Created");
+        } else {
+            System.out.println("Folder already created");
+        }
+    }
+
+    @Override
+    public Video save(Video video, MultipartFile file) {
+
+        try {
+
+            String filename = file.getOriginalFilename();
+            String contentType = file.getContentType();
+            InputStream inputStream = file.getInputStream();
+
+
+            String cleanFileName = StringUtils.cleanPath(filename);
+            String cleanFolder = StringUtils.cleanPath(DIR);
+
+            Path path = Paths.get(cleanFolder, cleanFileName);
+
+            Files.copy(inputStream, path, StandardCopyOption.REPLACE_EXISTING);
+
+            video.setContentType(contentType);
+            video.setFilePath(path.toString());
+            Video savedVideo = videoRepository.save(video);
+            //processing video
+            processVideo(savedVideo.getVideoId());
+
+            return savedVideo;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public Video get(String videoId) {
+
+        Video video = videoRepository.findById(videoId).orElseThrow(() -> new RuntimeException("Video not found"));
+        return video;
+    }
+
+    @Override
+    public Video getByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public List<Video> getAll() {
+        return videoRepository.findAll();
+    }
+
+    @Override
+    public String processVideo(String videoId) {
+
+        Video video = this.get(videoId);
+        String filePath = video.getFilePath();
+
+        Path videoPath = Paths.get(filePath);
+
+//        String output360p = HSL_DIR + videoId + "/360p/";
+//        String output720p = HSL_DIR + videoId + "/720p/";
+//        String output1080p = HSL_DIR + videoId + "/1080p/";
+
+        try {
+//            Files.createDirectories(Paths.get(output360p));
+//            Files.createDirectories(Paths.get(output720p));
+//            Files.createDirectories(Paths.get(output1080p));
+
+            Path outputPath = Paths.get(HSL_DIR, videoId);
+
+            Files.createDirectories(outputPath);
+
+            String ffmpegCmd = String.format(
+                    "ffmpeg -i \"%s\" -c:v libx264 -c:a aac -strict -2 -f hls -hls_time 10 -hls_list_size 0 -hls_segment_filename \"%s/segment_%%3d.ts\"  \"%s/master.m3u8\" ",
+                    videoPath, outputPath, outputPath
+            );
+
+//            StringBuilder ffmpegCmd = new StringBuilder();
+//            ffmpegCmd.append("ffmpeg  -i ")
+//                    .append(videoPath.toString())
+//                    .append(" -c:v libx264 -c:a aac")
+//                    .append(" ")
+//                    .append("-map 0:v -map 0:a -s:v:0 640x360 -b:v:0 800k ")
+//                    .append("-map 0:v -map 0:a -s:v:1 1280x720 -b:v:1 2800k ")
+//                    .append("-map 0:v -map 0:a -s:v:2 1920x1080 -b:v:2 5000k ")
+//                    .append("-var_stream_map \"v:0,a:0 v:1,a:0 v:2,a:0\" ")
+//                    .append("-master_pl_name ").append(HSL_DIR).append(videoId).append("/master.m3u8 ")
+//                    .append("-f hls -hls_time 10 -hls_list_size 0 ")
+//                    .append("-hls_segment_filename \"").append(HSL_DIR).append(videoId).append("/v%v/fileSequence%d.ts\" ")
+//                    .append("\"").append(HSL_DIR).append(videoId).append("/v%v/prog_index.m3u8\"");
+
+            System.out.println(ffmpegCmd);
+            //file this command
+            ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", ffmpegCmd);
+            processBuilder.inheritIO();
+            Process process = processBuilder.start();
+            int exit = process.waitFor();
+            if (exit != 0) {
+                throw new RuntimeException("video processing failed!!");
+            }
+
+            return videoId;
+
+        } catch (IOException ex) {
+            throw new RuntimeException("Video Processing Failed");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
